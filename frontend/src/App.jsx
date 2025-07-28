e8693b6d71f7ab15bcd562d77dffa0e067ef5af9
= <rashiidrashi5@gmail.com>
2025-07-28 10:10:27 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
+import Placeholder from './pages/Placeholder'
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
          <Route path='/cart' element={<Cart/>} />
          <Route path='/login' element={<Login/>}/>
          <Route path='/orders' element={<Orders/>}/>
+         <Route path='/place-order' element={<Placeholder/>}></Route>
       </Routes>
       <Footer />
     </div>
--- frontend/src/pages/Placeholder.jsx
+++ frontend/src/pages/Placeholder.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const Placeholder = () => {
   return (
-    <div>
-        
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+         
+         {/**-----------left side ----------------- */}
+         <div className='flex flex-col gap-4 w-full sm:max-w-[400px]'>
+             
+             <div className='text-xl sm:text-2xl my-3'>
+                 <Title text1={'DELIVERY'}  text2={'INFORMATION'}/>
+             </div>
+             <div className='flex gap-3'>
+                  <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full '  type="text" placeholder='First name'/>
+                  <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full '  type="text" placeholder='Last name'/>
+             </div>
+              <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full '  type="email" placeholder='Email address'/>
+              <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full '  type="text" placeholder='Street'/>
+         </div>
     </div>
   )
 }
