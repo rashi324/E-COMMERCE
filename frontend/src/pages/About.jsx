b4dd1c1e4e684b0315f4a275e40deb53bddaa75f
= <rashiidrashi5@gmail.com>
2025-07-31 13:19:18 +0530

    update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
@@ -15,8 +16,32 @@ const About = () => {
                <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, consequatur dolores! Beatae autem harum alias, illum eius exercitationem explicabo at saepe, corporis fugit illo soluta, ratione quos voluptatum libero rem! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, voluptatem!</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis est dolore dolor exercitationem error, at fugiat doloribus! Architecto, corrupti alias!</p>
+                  <b className='text-gray-800'>Our Mission</b>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni esse aperiam adipisci vero cum corporis officia eos voluptas impedit? Magnam assumenda temporibus quidem eligendi facilis blanditiis maxime exercitationem qui iste.</p>
                </div>
             </div>
+
+            <div className='text-xl py-4'>
+                 <Title text1={'WHY'} text2={'CHOOSE US'} />
+            </div>
+
+         <div className='flex flex-col md:flex-row text-sm mb-20'>
+              <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                    <b>Quality Assurance:</b>
+                    <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur adip. lorem10</p>
+              </div>
+              <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                    <b>Convenience:</b>
+                    <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipis</p>
+              </div>
+              <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+                    <b>Coustomer Service:</b>
+                    <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur t.</p>
+              </div>
+         </div>
+
+
+         <NewsletterBox />
 
     </div>
   )
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
     <div>
-        
+
+      <div className='text-center text-2xl pt-10 borde-t'>
+           <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+      
+        <div className='my-1 flex flex-col justify-center md:flex-row gap-8 mb-24'>
+            <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+            <div className='flex flex-col justify-center items-start gap-6' >
+                 <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                 <p className='text-gray-500'>Chennamangallur <br /> Mukkam , kozhikode</p>
+                 <p className='text-gray-500'>+91 9037362959 <br /> Email: bluegill@gmail.com</p>
+                 <p className='font-semibold text-xl text-gray-600'>Careers at Blue Gill</p>
+                 <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+                 <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+              
+            </div>
+            </div>
+
+            <NewsletterBox />
+
     </div>
   )
 }
